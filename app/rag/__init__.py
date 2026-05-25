1270a9c387aa0ad4abf3f629cedf5b52d6ca4ebd
Gargi Gupta <gargig469@gmail.com>
2026-05-25 19:02:57 +0530

    export rag repair helper

--- app/rag/__init__.py
+++ app/rag/__init__.py
@@ -3,15 +3,18 @@
 from .chunker import KnowledgeChunk, chunk_markdown_file
 from .embed import KnowledgeIndex, build_knowledge_index, load_knowledge_index, save_knowledge_index
 from .retrieve import RetrievalHit, hits_to_evidence_payloads, query_knowledge_index
+from .self_heal import RetrievalRepairReport, repair_retrieval_if_needed
 
 __all__ = [
     "KnowledgeChunk",
     "KnowledgeIndex",
     "RetrievalHit",
+    "RetrievalRepairReport",
     "build_knowledge_index",
     "chunk_markdown_file",
     "hits_to_evidence_payloads",
     "load_knowledge_index",
     "query_knowledge_index",
+    "repair_retrieval_if_needed",
     "save_knowledge_index",
 ]
