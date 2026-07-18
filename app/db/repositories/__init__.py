bca00455d69db861aa25173696039c2badf70090
Gargi Gupta <gargig469@gmail.com>
2026-07-19 01:36:20 +0530

    export worker repository helpers

--- app/db/repositories/__init__.py
+++ app/db/repositories/__init__.py
@@ -1,7 +1,14 @@
 from app.db.repositories.audit import AuditEventRepository
 from app.db.repositories.base import IdempotencyConflict, TenantRepository, TenantResourceNotFound
 from app.db.repositories.documents import DocumentRepository
-from app.db.repositories.jobs import ProcessingJobRepository
+from app.db.repositories.jobs import (
+    JobClaim,
+    JobClaimState,
+    JobReservation,
+    JobStateConflict,
+    ProcessingJobRepository,
+    resolve_worker_tenant,
+)
 from app.db.repositories.reviews import ReviewDecisionRepository
 from app.db.repositories.users import UserRepository
 
@@ -9,9 +16,14 @@ __all__ = [
     "AuditEventRepository",
     "DocumentRepository",
     "IdempotencyConflict",
+    "JobClaim",
+    "JobClaimState",
+    "JobReservation",
+    "JobStateConflict",
     "ProcessingJobRepository",
     "ReviewDecisionRepository",
     "TenantRepository",
     "TenantResourceNotFound",
     "UserRepository",
+    "resolve_worker_tenant",
 ]
