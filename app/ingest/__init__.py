11a5341a5d7732a89f8c49249c42b606760f5049
Gargi Gupta <gargig469@gmail.com>
2026-07-18 22:55:09 +0530

    export upload persistence tools

--- app/ingest/__init__.py
+++ app/ingest/__init__.py
@@ -13,15 +13,23 @@ from .validation import (
     UploadValidator,
     ValidatedUpload,
 )
+from .upload_service import (
+    QuarantinedUploadReceipt,
+    UploadPersistenceError,
+    persist_quarantined_upload,
+)
 
 __all__ = [
     "DocumentText",
     "IngestionError",
     "UploadKind",
+    "UploadPersistenceError",
+    "QuarantinedUploadReceipt",
     "UploadValidationError",
     "UploadValidator",
     "ValidatedUpload",
     "ocr_pdf_page",
+    "persist_quarantined_upload",
     "read_document_text",
     "supported_extensions",
 ]
